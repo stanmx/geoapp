a5d0feaedd3dcc7f25a5262763b9f18988c4a03e
stanmx <stanmx@gmail.com>
2013-07-03 14:54:36 -0700

    Ejemplo con geocoder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoapp::Application.config.secret_token = '73c920445b15b2d7c2405b91bcef4ec68d6f3cad2dfc618a06ba706a5e0b0699217524e77a5b7dd2894932ed03480406395ab6abd67b28ecbabd77af23c0a250'
